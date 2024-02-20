8984bb130cac06dcc244c642bddbc604de608ff1
fraberret <f.berretta02@gmail.com>
2024-02-20 16:55:41 +0100

    add button and input functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,24 @@
 //chiedere all'utente età e chilometri
-let userAge = document.getElementById("age")
-let userKm = document.getElementById("km")
+//let userAge = document.getElementById("age")
+//let userKm = document.getElementById("km")
 
 //calcolare prezzo
 // in base ai km
-let ticket_price = userKm * 0.21
+
 //in base all'età
 //SE utente ha < 18 anni -20%
 //ALTRIMENTI SE > 65 anni -40%
 
+
+
+
+//stampa risultato
+
+document.querySelector("button").addEventListener("click", function () {
+let userAge = document.getElementById("age").value
+let userKm = document.getElementById("km").value
+
+let ticket_price = userKm * 0.21
 
 if (userAge < 18) { //se minorenne
     
@@ -19,11 +29,7 @@ if (userAge < 18) { //se minorenne
     ticket_price=ticket_price-ticket_price*0.4   
 }
 
-//stampa risultato
-
-document.querySelector("button").addEventListener("click", function () {
-    
-    
+console.log(ticket_price.toFixed(2));
 })
 
 
