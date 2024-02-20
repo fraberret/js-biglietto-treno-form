6689a0971c5b599a28c211739cf6488cc9c816a4
fraberret <f.berretta02@gmail.com>
2024-02-20 18:31:04 +0100

    make "my tickets" appear when click

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
 
 document.querySelector("button").addEventListener("click", function () {
 //chiedere all'utente età e chilometri
-let userAge = document.getElementById("age").value
+let userAge =document.getElementById("age").value
 let userKm = document.getElementById("km").value
 let userName = document.getElementById("name").value
 
 let discount_20 = 0.2
 let discount_40 = 0.4
-let discountType = "Standard Price"
+let discountType = "Nessuno Sconto"
 
 //calcolare prezzo in base ai km
 let ticket_price = userKm * 0.21
@@ -15,22 +15,24 @@ let ticket_price = userKm * 0.21
 if (userAge < 18) { //se minorenne
     
     ticket_price=ticket_price-ticket_price*discount_20
-    discountType="20% off"
+    discountType="Sconto del 20%"
 
     //ALTRIMENTI SE > 65 anni -40%
 } else if (userAge >= 65) { //se over 65
    
     ticket_price=ticket_price-ticket_price*discount_40
-    discountType="40% off"   
+    discountType="Sconto del 40%"   
 }
 //stampa risultato
 let finalPrice = ticket_price.toFixed(2)
 
+document.getElementById("results").style.display="block"
+
 document.getElementById("name_tb").innerHTML= userName;
 document.getElementById("discount_tb").innerHTML= discountType
-document.getElementById("seat_tb").innerHTML= Math.random
-document.getElementById("code_tb").innerHTML= Math.random
-document.getElementById("price_tb").innerHTML= finalPrice
+document.getElementById("seat_tb").innerHTML= Math.floor(Math.random() * 31);
+document.getElementById("code_tb").innerHTML= Math.floor(Math.random() * 10000);
+document.getElementById("price_tb").innerHTML= `${finalPrice}€`
 
 })
 
