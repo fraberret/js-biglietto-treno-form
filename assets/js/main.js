03f59404be522fa2b6ed41eedb3658586b77a6d1
fraberret <f.berretta02@gmail.com>
2024-02-20 16:14:47 +0100

    fix program

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,32 +3,23 @@ let userAge = prompt("quanti anni hai?")
 let userKm = prompt("quanti km vuoi percorrere?")
 //calcolare prezzo
 // in base ai km
-let ticket = (userKm * 0.21)
+let ticket_price = userKm * 0.21
 //in base all'età
 //SE utente ha < 18 anni -20%
 //ALTRIMENTI SE > 65 anni -40%
-//ALTRIMENTI no sconto
 
-let finalPrice=ticket //prezzo ticket base
 
 if (userAge < 18) { //se minorenne
-    console.log(userAge, userKm);
-    let sconto = (ticket * 20 / 100)
-    let finalPrice = (ticket - sconto)
-    console.log(finalPrice.toFixed(2));//prezzo finale
+    
+    ticket_price=ticket_price-ticket_price*0.2
 
 } else if (userAge >= 65) { //se over 65
-    console.log(userAge, userKm);
-    let sconto = (ticket * 40 / 100)
-    let finalPrice = (ticket - sconto)
-    console.log(finalPrice.toFixed(2));//prezzo finale
-    
-} else{ //se nessuno dei due
-    console.log(userAge, userKm);
-    console.log(finalPrice.toFixed(2));//prezzo finale
+   
+    ticket_price=ticket_price-ticket_price*0.4   
 }
 
-
+//stampa risultato
+console.log(ticket_price.toFixed(2));
 
 
 
